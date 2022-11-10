42ab7af6471c9b7e88578dcf94df98161036ca19
Oleksandr Nikolaychuk <santifick.nik@gmail.com>
2022-11-10 04:59:26 +0200

    Add project files.

---
+++ TRSPO P/module1.py
@@ -0,0 +1,26 @@
+
+import threading
+
+
+
+def collatz(n):
+    while n > 1:
+        print(n, end=' ')
+        if (n % 2):
+            # n is odd
+            n = 3 * n + 1
+        else:
+            # n is even
+            n = n // 2
+    print(1, end='')
+
+
+n = int(input('Enter n: '))
+
+
+i=1
+while i <= n :
+    print('\nSequence: ', end=' ')
+    t2 = threading.Thread(target=collatz(i))
+    t2.start()
+    i = i + 1
